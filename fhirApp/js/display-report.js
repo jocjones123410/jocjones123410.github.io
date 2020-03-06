f3434a47ea650baa83e9d29e99ea9244501e3900
jocjones123410 <52826933+jocjones123410@users.noreply.github.com>
2020-03-06 10:53:02 -0500

    Update display-report.js

--- fhirApp/js/display-report.js
+++ fhirApp/js/display-report.js
@@ -248,19 +248,23 @@ function populateMedicationsTable(medicationStatementBundle){
 
 function populateCoverageSection(coverageBundle){
 	let type = '';
-	let coverage = coverage.entry;
+	let relationship = '';
+	let period = '';
+	let detailName = '';
+	
+	let coverage = coverageBundle.entry;
 	if(coverage.type && coverage.type.coding && coverage.type.coding[0].display){
 		type = coverage.type.coding[0].display;
 	}
 	populateDataItem('coverageType',type);
 		
-	let relationship = '';
+	
 	if(coverage.relationship && coverage.relationship.coding && coverage.relationship.coding[0].code){			
 		relationship = divWrapper(createLabel('Relationship: ') + coverage.relationship.coding[0].code);
 	}
 	populateDataItem('coverageRelationship',relationship);
 	
-	let period = '';
+	
 	if(coverage.period){
 		period = coverage.period.start + " - " + coverage.period.end;
 	}
@@ -268,7 +272,7 @@ function populateCoverageSection(coverageBundle){
 	
 	//let detailsLabel = divWrapper(createLabel('Details: '));
 	//setDomElement('coverageId', type + relationship + period + detailsLabel);
-	let detailName = '';
+	
 	if(coverage.class){
 		for(let i=0;i<coverage.class.length;i++){
 			if(coverage.class[i].name){
